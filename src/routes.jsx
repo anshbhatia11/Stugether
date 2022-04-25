4957a19ac75562c7d192b9976578574d0943a41b
Ansh Bhatia <bansh5081@gmail.com>
2022-04-25 12:50:05 +0530

    Source update

---
+++ src/routes.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+import Chat from "./pages/Chat/Chat";
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/login" component={Login} />
+      <Route path="/TRS"  />
+      <Route path="/channel/:channelId">
+        <Chat />
+      </Route>
+      <Route path="/" exact={true} component={Home} />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
