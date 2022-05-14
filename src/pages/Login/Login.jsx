b34945dc12f9a0509b5ffa179a3ec8e228ab1244
Ansh Bhatia <bansh5081@gmail.com>
2022-05-14 10:13:30 +0530

    Update Login.jsx

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -25,7 +25,7 @@ function Login() {
         <p>Bringing Students Together!</p>
         <Button onClick={login}>Log in with Google</Button>
         <br></br>
-        <Button onClick={login}>Log in with Email Id</Button>
+        {/* <Button onClick={login}>Log in with Email Id</Button> */}
       </div>
     </div>
   );
